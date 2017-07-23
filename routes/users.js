902d52614f3381e22120cd790d8a44209f0f8068
JammiM <jammim19@gmail.com>
2017-07-23 23:06:38 +0100

    Added some form validation and fixed a bootstrap bug

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -16,6 +20,55 @@ router.get('/register', function(req, res, next) {
 // Displays the login page
 router.get('/login', function(req, res, next) {
   res.render('login', {title: 'Login'});
+});
+
+
+// Uploads the profile picture
+router.post('/register', upload.single('profileImage') ,function(req, res, next) {
+
+  let name = req.body.name;
+  let email = req.body.email;
+  let userName = req.body.userName;
+  let password = req.body.password;
+  let password2 = req.body.password2;
+
+  if(req.file){
+  	console.log('Uploading file...');
+
+  	// Grabs the file name
+  	let profileImage = req.file.filename;
+
+
+  } else {
+  	console.log('No file uploaded...');
+
+  	// If no image is uploaded.
+  	let profileImage = 'noimage.jpg';
+  }// else
+
+
+  // Form validation
+  req.checkBody('name', 'Name field is required').notEmpty();
+  req.checkBody('email', 'Email field is required').notEmpty();
+  req.checkBody('userName', 'User Name field is required').notEmpty();
+  req.checkBody('password', 'Password field is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+
+// Check for errors
+let errors = req.validationErrors();
+if(errors){
+	//console.log('Errors detected');
+	res.render('register', {
+		errors: errors
+	})
+} else {
+	console.log('No Errors detected');
+}
+
+
+
+
 });
 
 
